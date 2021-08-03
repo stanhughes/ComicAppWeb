c0781703158865a2116823df1a71ab957f8d34f0
Stan <hughes.stan@gmail.com>
2021-08-03 13:43:04 -0400

    Image is displaying but not a background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import background from './img/background/marcin-lukasik-uYpOYyJdhRE-unsplash.jpeg'
 
 function App() {
   return (
     <div className="App">
+      <div style={{ backgroundImage: `url(${background})`}}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Mike Mercer</h1>
+        <img src={background} />
+        <img src={background} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +19,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
+      </div>
     </div>
   );
 }
